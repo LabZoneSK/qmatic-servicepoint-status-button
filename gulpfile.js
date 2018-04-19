500f84a281f89b9ad1eb6c251f17a1be5d96a983
Martin Starosta <martin.starosta83@gmail.com>
2018-04-19 14:14:59 +0200

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 const autoprefixer = require('gulp-autoprefixer');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
 const cssnano = require('gulp-cssnano');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const imagemin = require('gulp-imagemin');
 const runSequence = require('run-sequence');
 const sass = require('gulp-sass');
 const zip = require('gulp-zip');
 
+gulp.task('clean', () => {
+  return gulp.src('./build', {
+      read: false
+    })
+    .pipe(clean());
+});
+
 gulp.task('copy', () => {
-  gulp.src(['./src/*.html', './src/i18n/*.*', './src/config.xml'])
-  .pipe(gulp.dest('./build'))
+  gulp.src(['./src/*.html', './src/*.xml'])
+    .pipe(gulp.dest('./build'));
+
+  return gulp.src('./src/i18n/*.*')
+    .pipe(gulp.dest('./build/i18n'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('./src/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('build/images'))
 });
 
 gulp.task('scripts', () => {
@@ -21,7 +40,7 @@ gulp.task('styles', () => {
     .pipe(sass({
       style: 'expanded'
     }))
-      .on('error', gutil.log)
+    .on('error', gutil.log)
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
@@ -38,7 +57,7 @@ gulp.task('create-wgt', () => {
 
 gulp.task('build', (cb) => {
   runSequence(
-    ['copy', 'scripts','styles'],
+    'clean', ['copy', 'scripts', 'styles', 'images'],
     'create-wgt',
     cb
   )
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,16 +1,63 @@
-var controller = (function($) {
+var controller = (function ($) {
   var self = this;
   var wwClient = qmatic.webwidget.client;
   var wwRest = qmatic.connector.client;
 
+  const branchId = wwClient.getBranchId();
+  const unitId = wwClient.getUnitId();
+
+  function initializeWidget(widgetConfiguration) {
+    const servicePointMI = wwRest.getServicePointData(branchId);
+    const openWSP = servicePointMI.filter(function(servicePoint) {
+      if (servicePoint.workProfileName) {
+        return (servicePoint.workProfileName.indexOf(widgetConfiguration.workprofileMatch) > -1);
+      } else {
+        return false;
+      }
+    });
+
+    if(openWSP.length > 0) {
+      console.log('There is open workstation matching string ' + widgetConfiguration.workprofileMatch);
+      $('.widget-container').click(function(event) {
+        wwRest.createVisitByUnitId(unitId, {
+          services: [widgetConfiguration.service]
+        }, 'LAST');
+        wwClient.switchHostPage(widgetConfiguration.ticketRedirectPage, false);
+      });
+    } else {
+      console.log('There is no open workstation matching string ' + widgetConfiguration.workprofileMatch);
+      $('.widget-container').click(function(event) {
+        wwClient.switchHostPage(widgetConfiguration.closedRedirectPage, false);
+      });
+    }
+  };
+
   return {
-    onLoaded: function(configuration) {
-      console.log('On loaded');
+    onLoaded: function (configuration) {
+      const attr = configuration.attributes;
+      const attribParser = new qmatic.webwidget.AttributeParser(attr || {});
+
+      const widgetConfiguration = {
+        closedRedirectPage: attribParser.getString('closed_page'),
+        ticketRedirectPage: attribParser.getString('ticket_page'),
+        workprofileMatch: attribParser.getString('workprofile_match'),
+        service: attribParser.getString('service')
+      }
+
+      const imageSrc =  attribParser.getImageUrl('button_img');
+      if(imageSrc) {
+        $('.widget-container img.button').attr('src', imageSrc);
+      }
+
+      initializeWidget(widgetConfiguration);
+      setInterval(function () {
+        initializeWidget(widgetConfiguration);
+      }, 50000);
     },
 
-    onLoadError: function(message) {
+    onLoadError: function (message) {
       console.error(message);
     }
   };
-  
+
 })(jQuery);
